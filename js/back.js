402af04cab59b3700765a76237bc2c0ac42c497f
yasmin595 <yasminsuborna8@gmail.com>
2025-03-02 20:01:58 +0600

    added problem fixed

--- js/back.js
+++ js/back.js
@@ -1,4 +1,4 @@
 document.getElementById("back-button").addEventListener("click", function(event){
     event.preventDefault();
-    window.location.href="../index.html";
+    window.location.href="./index.html";
 })
